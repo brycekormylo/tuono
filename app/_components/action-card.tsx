b08d34b9d67d5a8c98455f93dc9c7ecd9d7a1803
Bryce Kormylo <brycekormylo@outlook.com>
2024-12-26 12:44:58 -0800

    stability updates

--- app/_components/action-card.tsx
+++ app/_components/action-card.tsx
@@ -1,3 +1,3 @@
-export default function ActionCard() {
-  return <div className="w-40 h-40 bg-gray-400 rounded-xl"></div>;
-}
+// export default function ActionCard() {
+//   return <div className="w-40 h-40 bg-gray-400 rounded-xl"></div>;
+// }
